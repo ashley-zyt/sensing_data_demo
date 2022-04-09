1c1ff87fa9159640908029da61c6725ee3d3d773
zhaoyuting <zhaoyuting@china-revival.com>
2022-04-09 07:06:02 -0700

    1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   include DeviseTokenAuth::Concerns::User
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable
   before_validation :uid_populate, on: :create
 
   def confirmed_at
