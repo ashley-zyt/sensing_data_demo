db91b7204f63796f284fe13b7446ad258471cf5d
zhaoyuting <zhaoyuting@china-revival.com>
2022-04-09 08:21:48 -0700

    1

--- app/models/senseing_data.rb
+++ app/models/senseing_data.rb
@@ -1,6 +1,8 @@
 class SenseingData < ActiveRecord::Base
 	self.table_name = "senseing_datas"
 	def create_datas
-		
+		for i in 1..100
+			SenseingData.new(data_type:rand(2),num_int_one: i, num_int_two: i, num_string_one: i, num_string_two: i).save
+		end
 	end
 end
